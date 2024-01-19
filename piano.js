dbf9ad137b5fdf48c0bfe53c79cd605256a8c643
Ben Vessely <benvessely@gmail.com>
2024-01-18 21:53:58 -0800

    Should have AudioBuffer for c4 created

--- piano.js
+++ piano.js
@@ -3,7 +3,6 @@ const noteNames = [
 ];
 
 
-
 // Code below to check to make sure DOM content loaded before adding event listeners.
 if (document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', () => {
@@ -51,28 +50,31 @@ function ready() {
 } 
 
 
+// Creates the AudioBuffers from the mp3 Files, one for every note in array noteNames
 function createAudioBuffers(audioContext) { 
-
     // Array to hold all of the arrayBuffers
     let returnArray = [];
     // const c4AudioURL = "https://corsproxy.io/https://github.com/benvessely/virtual-piano/blob/main/sounds/c4-virtual-piano.mp;";
     // let c4ArrayBuffer; 
-    const c4AudioFetch = fetch('sounds/c4-virtual-piano.mp3') 
+    const noteAudioFetch = fetch('sounds/c4-virtual-piano.mp3') 
         .then((response) => {
-            c4ArrayBufferPromise = response.arrayBuffer();
-            return c4ArrayBufferPromise
-            // console.log(`c4ArrayBuffer = ${c4ArrayBuffer}`); //DB 
-            // returnArray.push(c4ArrayBuffer);
-            // console.log(`returnArray = ${returnArray}`); //DB 
-            // return returnArray;
+            noteArrayBufferPromise = response.arrayBuffer();
+            return noteArrayBufferPromise;
         })
-        .then((c4arrayBuffer) => {
-            returnArray.push(c4arrayBuffer);
+        // Once the .arrayBuffer() asynchronous method has resolved, the .then occurs, calling the arrow function within that pushes the arraybuffer into our larger scoped array of arraybuffers. 
+        .then((newArrayBuffer) => { 
+            newAudioBuffer = audioContext.decodeAudioData(newArrayBuffer);
+            return newAudioBuffer;
+        }) 
+        .then((newAudioBuffer) => {
+            returnArray.push(newAudioBuffer);
         })
         .catch((error) => { 
             console.error(error); 
-            return 
+            return; 
         })
+    
+    return returnArray;
         
     // Possibly working code below? 
     // const c4AudioURL = "https://github.com/benvessely/virtual-piano/blob/main/sounds/c4-virtual-piano.mp3";
@@ -88,8 +90,6 @@ function createAudioBuffers(audioContext) {
     //     audioContext.sampleRate * c4Audio.duration,
     //     audioContext.sampleRate
     // ); 
-
-    return returnArray;
 }
 
 
