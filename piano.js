b5a05843e46f6579617554d4de5c9c5f7d11bfd4
Ben Vessely <benvessely@gmail.com>
2024-01-19 17:32:00 -0800

    Working through promises in createAudioBuffers, going to try to make ready() async

--- piano.js
+++ piano.js
@@ -12,33 +12,6 @@ if (document.readyState === 'loading') {
     ready();
 }
 
-// function startButton() {
-//     const startButton = document.createElement('button'); 
-//     startButton.setAttribute('type', 'button'); 
-//     startButton.addEventListener('click', ready()); 
-// }
-
-function GenerateAudioPlayer(audioBuffers, audioContext) {
-    const audioPlayer = { 
-        APAudioBuffers: audioBuffers, 
-        APAudioContext: audioContext, 
-        createPrimaryGain: function(gainValue) { 
-            this.primaryGainControl = this.APAudioContext.createGain(); 
-            this.primaryGainControl.gain.setValueAtTime(gainValue, 0); 
-            this.primaryGainControl.connect(audioContext.destination); 
-            console.log("At end of createPrimaryGain body"); //DB 
-        },
-        playNote: function() { 
-            console.log("We are in the playNote body"); 
-            const c4Source = this.APAudioContext.createBufferSource(); 
-            c4Source.buffer = this.APAudioBuffers[0]; 
-            c4Source.connect(this.primaryGainControl); 
-            c4Source.start(); 
-            setTimeout(() => c4Source.stop(), 2000); 
-        }
-    }
-    return audioPlayer
-}
 
 function ready() { 
     // const c4Audio = new Audio('sounds/c4-virtual-piano.mp3');
@@ -46,17 +19,13 @@ function ready() {
     const audioContext = new (window.AudioContext || window.webkitAudioContext)();
     // let loadedMetaData = false; 
     const audioBuffers = CreateAudioBuffers(audioContext); 
+    // DB below 
+    audioBuffers.then((data) => { 
+        console.log(`Testing audioBuffers promise, we have data = ${data}`); 
+    }); //
     const audioPlayer = GenerateAudioPlayer(audioBuffers, audioContext); 
     audioPlayer.createPrimaryGain(.50);  
-
-    // const primaryGainControl = audioContext.createGain(); 
-    // primaryGainControl.gain.setValueAtTime(.50, 0); 
-    // primaryGainControl.connect(audioContext.destination); 
-
-    // c4Audio.addEventListener('loadedmetadata', function () {
-    //     loadedMetaData = true;
-    //     checkReadyState();
-    // });
+    audioPlayer.c4NoteCreation(); 
 
     // Adds event listeners to buttons 
     var pianoKeys = document.querySelectorAll('.btn-key-white, .btn-key-black');
@@ -76,39 +45,54 @@ function ready() {
 } 
 
 
+function GenerateAudioPlayer(audioBuffers, audioContext) {
+    const audioPlayer = { 
+        APAudioBuffers: audioBuffers, 
+        APAudioContext: audioContext, 
+        createPrimaryGain: function(gainValue) { 
+            this.primaryGainControl = this.APAudioContext.createGain(); 
+            this.primaryGainControl.gain.setValueAtTime(gainValue, 0); 
+            this.primaryGainControl.connect(audioContext.destination); 
+            console.log("At end of createPrimaryGain body"); //DB 
+        },
+        c4NoteCreation: function() {
+            console.log(`this.APAudioBuffers[0] = ${this.APAudioBuffers[0]} at the start of c4NoteCreation`); 
+            this.c4Source = this.APAudioContext.createBufferSource();
+            this.c4Source.buffer = this.APAudioBuffers[0]; 
+            this.c4Source.connect(this.primaryGainControl);
+        },
+        playNote: function() { 
+            console.log(`this.c4Source = ${this.c4Source} at the start of playNote`); //DB 
+            this.c4Source.start(); 
+            setTimeout(() => this.c4Source.stop(), 2000); 
+            console.log("We are at end of in the playNote body"); //DB
+            // Testing code below 
+            // const c4Source = this.APAudioContext.createBufferSource(); 
+            // c4Source.buffer = this.APAudioBuffers[0]; 
+            // c4Source.connect(this.primaryGainControl); 
+            // c4Source.start(); 
+            // setTimeout(() => c4Source.stop(), 2000); 
+        }
+    }
+    return audioPlayer
+}
+
+
 // Creates the AudioBuffers from the mp3 Files, one for every note in array noteNames
-function CreateAudioBuffers(audioContext) { 
+async function CreateAudioBuffers(audioContext) { 
     // Array to hold all of the arrayBuffers
-    let returnArray = [];
-    // const c4AudioURL = "https://corsproxy.io/https://github.com/benvessely/virtual-piano/blob/main/sounds/c4-virtual-piano.mp;";
-    // let c4ArrayBuffer; 
-    const noteAudioFetch = fetch('sounds/c4-virtual-piano.mp3') 
-        .then((response) => {
-            noteArrayBufferPromise = response.arrayBuffer();
-            return noteArrayBufferPromise;
-        })
-        // Once the .arrayBuffer() asynchronous method has resolved, the .then occurs, calling the arrow function within that pushes the arraybuffer into our larger scoped array of arraybuffers. 
-        .then((newArrayBuffer) => { 
-            newAudioBuffer = audioContext.decodeAudioData(newArrayBuffer);
-            return newAudioBuffer;
-        }) 
-        .then((newAudioBuffer) => {
-            returnArray.push(newAudioBuffer);
-        })
-        .catch((error) => { 
-            console.error(error); 
-            return; 
-        })
+    const returnArray = [];
+    try { 
+        const response = await fetch('sounds/c4-virtual-piano.mp3');
+        const newArrayBuffer = await response.arrayBuffer();
+        const newAudioBuffer = await audioContext.decodeAudioData(newArrayBuffer);
+        returnArray.push(newAudioBuffer);
+    } catch(error) { 
+        console.error(error); 
+        return; 
+    } 
     
     return returnArray;
-        
-    // Possibly working code below? 
-    // const c4AudioURL = "https://github.com/benvessely/virtual-piano/blob/main/sounds/c4-virtual-piano.mp3";
-    // const c4AudioFetch = fetch(c4AudioURL, {mode: 'no-cors'})
-    //     .catch((error) => { 
-    //         console.error(error); 
-    //         return 
-    //     })
 
     // Code to create buffer 
     // const c4Buffer = audioContext.createBuffer(
