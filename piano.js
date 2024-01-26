b38bfe81e4708555e87bea931c64ce8eec55b925
Ben Vessely <benvessely@gmail.com>
2024-01-26 12:46:50 -0800

    CreateAudioBuffers creates AudioBuffers for all notes now

--- piano.js
+++ piano.js
@@ -1,5 +1,5 @@
 const noteNames = [
-    "c4"
+    "c3", "cSharp3", "d3", "dSharp3", "e3", "f3", "fSharp3", "g3", "gSharp3", "a3", "aSharp3", "b3", "c4"
 ];
 
 
@@ -25,13 +25,13 @@ async function ready() {
     //     console.log(`Testing audioBuffers promise, we have data = ${data}`); 
     // }); //
     const audioPlayer = GenerateAudioPlayer(audioBuffers, audioContext); 
-    audioPlayer.createPrimaryGain(.9);  
+    audioPlayer.createPrimaryGain();  
 
     // Adds event listeners to buttons 
     var pianoKeys = document.querySelectorAll('.btn-key-white, .btn-key-black');
     for (var i = 0; i < pianoKeys.length; i++) {
         var button = pianoKeys[i];
-        button.addEventListener('mousedown', () => { 
+        button.addEventListener('mousedown', () => {  
             audioPlayer.playNote(); 
             // Testing code below 
             // const c4Source = audioContext.createBufferSource(); 
@@ -49,16 +49,12 @@ function GenerateAudioPlayer(audioBuffers, audioContext) {
     const audioPlayer = { 
         audioBuffers: audioBuffers, 
         audioContext: audioContext, 
-        createPrimaryGain(gainValue) { 
+        createPrimaryGain(gainValue=1) { 
             this.primaryGainControl = this.audioContext.createGain(); 
             this.primaryGainControl.gain.setValueAtTime(gainValue, 0); 
             this.primaryGainControl.connect(audioContext.destination); 
             // console.log("At end of createPrimaryGain body"); //DB 
         },
-        // createNoteGain: function(bufferSource, gainValue) {
-            
-            
-        // },
         c4NoteCreation() {
             // console.log(`this.audioBuffers[0] = ${this.audioBuffers[0]} at the start of c4NoteCreation`); 
             c4Note = this.audioContext.createBufferSource();
@@ -69,6 +65,7 @@ function GenerateAudioPlayer(audioBuffers, audioContext) {
             console.log(`At start of playNote`); //DB 
             const c4Note = this.c4NoteCreation(); 
 
+            // Create gain specific to the new note that was just created
             const noteGain = this.audioContext.createGain(); 
             noteGain.gain.setValueAtTime(1, this.audioContext.currentTime); 
             c4Note.connect(noteGain); 
@@ -84,10 +81,10 @@ function GenerateAudioPlayer(audioBuffers, audioContext) {
 
             // Default behavior of piano is to play note for x time, then do exponential decay after that 
             setTimeout(() => { 
-                // WHY DOES THIS LINE FIX THINGS??? 
-                // noteGain.gain.setValueAtTime(1, 0); 
+                // WHY DOES THIS LINE FIX THINGS??? TODO CHECK THIS 
+                noteGain.gain.setValueAtTime(1, 0); 
                 // releaseTime is time for noteGain to go to 0
-                const releaseTime = 3; 
+                const releaseTime = 2; 
                 noteGain.gain.exponentialRampToValueAtTime(
                     .001, 
                     this.audioContext.currentTime + releaseTime
@@ -107,18 +104,28 @@ function GenerateAudioPlayer(audioBuffers, audioContext) {
 }
 
 
-// Creates the AudioBuffers from the mp3 Files, one for every note in array noteNames
 async function CreateAudioBuffers(audioContext) { 
-    // Array to hold all of the arrayBuffers
+    // Creates the AudioBuffers from the mp3 Files, one for every note in array noteNames
     try { 
-        // console.log("At start of try block in CreateAudioBuffers"); 
+        // console.log("At start of try block in CreateAudioBuffers"); //DB 
+
+        // Array to hold all of the AudioBuffers 
         const returnArray = [];
-        const response = await fetch('sounds/c4-virtual-piano.mp3');
-        const newArrayBuffer = await response.arrayBuffer();
-        const newAudioBuffer = await audioContext.decodeAudioData(newArrayBuffer);
-        returnArray.push(newAudioBuffer);
+
+        // All audio files should be fetched, converted to AudioBuffers, and added to returnArray after this loop 
+        for (const element of noteNames) { 
+            const soundFile = 'sounds/' + element + 'VirtualPiano.mp3'; 
+            // console.log(`soundFile = ${soundFile}`); //DB 
+            const response = await fetch(soundFile);
+            const newArrayBuffer = await response.arrayBuffer();
+            const newAudioBuffer = await audioContext.decodeAudioData(newArrayBuffer);
+            returnArray.push(newAudioBuffer)
+        }
+        // console.log(`returnArray.length = ${returnArray.length}`); //DB 
+
         return returnArray;
-    } catch(error) { 
+    } 
+    catch(error) { 
         console.error(error); 
         return; 
     } 
